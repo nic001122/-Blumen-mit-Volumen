d301e00f7bb168b9c5008d1d718572d4b122a133
nic001122 <123776405+nic001122@users.noreply.github.com>
2024-05-30 14:07:10 +0200

    intialer comitone

---
+++ BlumenMitVolumen/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    private Vector3 Origin;
+    private Vector3 Difference;
+    private Vector3 ResetCamera;
+
+    private bool drag = false;
+
+    private void Start()
+    {
+        ResetCamera = Camera.main.transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            Difference = (Camera.main.ScreenToWorldPoint(Input.mousePosition)) - Camera.main.transform.position;
+            if (drag == false)
+            {
+                drag = true;
+                Origin = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            }
+        }
+        else
+        {
+            drag = false;
+        }
+
+        if (drag)
+        {
+            Camera.main.transform.position = Origin - Difference;
+
+        }
+
+    }
+}
