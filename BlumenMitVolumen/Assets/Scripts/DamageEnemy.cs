e16bd079e2c077acf66875a930b559645c485053
nic001122 <123776405+nic001122@users.noreply.github.com>
2024-05-31 14:11:35 +0200

    Felix ist ein Big LLL

--- BlumenMitVolumen/Assets/Scripts/DamageEnemy.cs
+++ BlumenMitVolumen/Assets/Scripts/DamageEnemy.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DamageEnemy : MonoBehaviour
 {
@@ -41,6 +42,7 @@ public class DamageEnemy : MonoBehaviour
         }
         if (currentLife <= 0)
         {
+            SceneManager.LoadScene(2);
             Destroy(gameObject);
         }
     }
