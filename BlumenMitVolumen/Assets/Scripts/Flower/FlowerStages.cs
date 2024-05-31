7527da97e7bc48870fcf7be92c07d511ccd3224a
NewByList <tonatiuh@gmx.de>
2024-05-31 16:16:31 +0200

    added the flower's stages

---
+++ BlumenMitVolumen/Assets/Scripts/Flower/FlowerStages.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerStages : MonoBehaviour
+{
+    [SerializeField] DamageEnemy damageEnemy;
+    [SerializeField] float firstStageChange = 0.75f;
+    [SerializeField] float secondStageChange = 0.25f;
+
+    [SerializeField] SpriteRenderer flowerSr;
+    [SerializeField] Sprite badFlower;
+    [SerializeField] Sprite worstFlower;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+        if(damageEnemy.currentLife <= damageEnemy.maxLife * firstStageChange
+            && damageEnemy.currentLife > damageEnemy.maxLife * secondStageChange)
+        {
+            flowerSr.sprite = badFlower;
+        }
+        else if(damageEnemy.currentLife <= damageEnemy.maxLife * secondStageChange)
+        {
+            flowerSr.sprite = worstFlower;
+        }
+    }
+}
