33c6063537b0fd0ac4fbd2e95d2d0455aed61b6f
NewByList <tonatiuh@gmx.de>
2024-06-01 13:16:57 +0200

    added insect always turning in the direction of flower

--- BlumenMitVolumen/Assets/Scripts/Enemy/EnemyLookAtFlower.cs
+++ BlumenMitVolumen/Assets/Scripts/Enemy/EnemyLookAtFlower.cs
@@ -1,31 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyLookAtFlower : MonoBehaviour
 {
     [SerializeField] Transform enemy;
-    Vector2 enemyPos;
-    Vector2 flowerPos;
-    float enemyAngle;
-
-    void Awake()
-    {
-        flowerPos = new Vector2(0, 0);
-    }
+    [SerializeField] float enemyAngle;
 
     // Update is called once per frame
     void Update()
     {
-        enemyPos = gameObject.transform.position;
-        
-        enemyAngle = Mathf.Atan2(enemyPos.x, enemyPos.y) * Mathf.Rad2Deg;
-        Quaternion rotation = Quaternion.AngleAxis(enemyAngle - 90, Vector3.forward);
+        enemyAngle = Mathf.Atan2(transform.position.y, transform.position.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(enemyAngle, Vector3.forward);
         enemy.rotation = rotation;
-        if(Input.GetKeyDown(KeyCode.C))
-        {
-            Debug.Log(rotation);
-        }
     }
 }
