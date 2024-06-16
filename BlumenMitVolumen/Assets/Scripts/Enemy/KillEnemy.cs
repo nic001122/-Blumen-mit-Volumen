11e96eba8dbfb2c8f1363e63aa8162c73ee16083
NewByList <tonatiuh@gmx.de>
2024-06-16 18:25:01 +0200

    Minerals now have a spawn rate

--- BlumenMitVolumen/Assets/Scripts/Enemy/KillEnemy.cs
+++ BlumenMitVolumen/Assets/Scripts/Enemy/KillEnemy.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 
 public class KillEnemy : MonoBehaviour
 {
@@ -11,6 +9,9 @@ public class KillEnemy : MonoBehaviour
 
     [SerializeField] float enemyHP = 10f;
     [SerializeField] float onClickDamage = 2.5f;
+
+    int roll;
+    [SerializeField] int spawnChance = 35; // int 1-100
 
     private void OnMouseDown()
     {
@@ -23,8 +24,22 @@ public class KillEnemy : MonoBehaviour
 
     void EnemyDeath()
     {
-        Instantiate(mineralsPrefab, transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)));
+        CheckMineralSpawnRate();
         Instantiate(particleSys, transform.position, Quaternion.identity);
         Destroy(gameObject);
+    }
+
+    void CheckMineralSpawnRate()
+    {
+        roll = Random.Range(1, 101);
+        if(roll <= spawnChance)
+        {
+            SpawnMineral();
+        }
+    }
+
+    void SpawnMineral()
+    {
+        Instantiate(mineralsPrefab, transform.position, Quaternion.Euler(0, 0, Random.Range(0, 360)));
     }
 }
